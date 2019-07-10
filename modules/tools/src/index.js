d42647ca704969a66c130fbeec4959ce294f2762
Maxim Samoilov <nitive@icloud.com>
2019-07-11 00:51:58 +0300

    Rename folders

--- modules/tools/src/index.js
+++ modules/tools/src/index.js
@@ -1,10 +1,11 @@
 const path = require('path')
 const realFs = require('fs')
 const { compile } = require('handlebars')
+const prettier = require('prettier')
 const e2eToolsPackage = require('../package.json')
 
 /**
- * @typedef {'mkdirSync' | 'statSync' | 'writeFileSync'} UsedFsMethods
+ * @typedef {'mkdirSync' | 'statSync' | 'writeFileSync' | 'readFileSync'} UsedFsMethods
  * @typedef {Pick<typeof import('fs'), UsedFsMethods>} FileSystem
  *
  * @typedef {{
@@ -16,7 +17,7 @@ const e2eToolsPackage = require('../package.json')
 /**
  *
  * @param {string} pathToFile Path to destination file
- * @param {{ fs: FileSystem }} templateOptions
+ * @param {{ fs: FileSystem }} options
  * @param {object=} templateContext
  */
 function createWithTemplate(pathToFile, { fs }, templateContext) {
@@ -26,6 +27,18 @@ function createWithTemplate(pathToFile, { fs }, templateContext) {
   const render = compile(realFs.readFileSync(pathToTemplate, { encoding: 'utf8' }))
 
   fs.writeFileSync(pathToFile, render(templateContext))
+}
+
+/**
+ * @param {{ fs: FileSystem }} templateOptions
+ * @param {(packageJsonContent: object) => object} update function which updates package.json content
+ */
+function updatePackageJson({ fs }, update) {
+  const packageJsonFile = fs.readFileSync('/e2e-tests/package.json', { encoding: 'utf8' })
+  const newPackageJsonFile = prettier.format(JSON.stringify(update(JSON.parse(packageJsonFile))), {
+    parser: 'json',
+  })
+  fs.writeFileSync('e2e-tests/package.json', newPackageJsonFile, { encoding: 'utf-8' })
 }
 
 /**
@@ -62,6 +75,13 @@ const addNightwatchCommand = ({ fs }) => ({
     )
 
     createWithTemplate('e2e-tests/nightwatch/screenshots/.gitignore', { fs })
+    updatePackageJson({ fs }, content => ({
+      ...content,
+      devDependencies: {
+        ...content.devDependencies,
+        '@csssr/e2e-tools-nightwatch': '*',
+      },
+    }))
   },
 })
 
