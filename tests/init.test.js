45463b83396091dfcc55ec3ce87381d8567c1043
Maxim Samoilov <nitive@icloud.com>
2019-07-10 16:28:30 +0300

    Test package.json content

--- tests/init.test.js
+++ tests/init.test.js
@@ -46,4 +46,21 @@ describe('init command', () => {
       "
     `)
   })
+
+  it('should create package.json', async () => {
+    const { fs } = await run('init')
+
+    const packageJsonFile = fs.readFileSync('/e2e-tests/package.json', { encoding: 'utf8' })
+    expect(JSON.parse(packageJsonFile)).toMatchInlineSnapshot(`
+      Object {
+        "devDependencies": Object {
+          "@csssr/e2e-tools": "~0.1.0",
+          "husky": "^3.0.0",
+        },
+        "husky": Object {},
+        "prettier": "@csssr/e2e-tools/prettier",
+        "private": true,
+      }
+  `)
+  })
 })
