7ba881507348049aa7cb34172cddf80bc1cdcef3
Maxim Samoilov <nitive@icloud.com>
2021-02-11 13:00:10 +0300

    fix: Do not use browser.sessions API because it does not work with Selenium Grid

--- modules/nightwatch/src/nightwatch-settings/setup-hooks.js
+++ modules/nightwatch/src/nightwatch-settings/setup-hooks.js
@@ -70,20 +70,14 @@ exports.setupHooksInternal = () => {
       })
     }
 
-    // browser.session is broken
-    // https://github.com/nightwatchjs/nightwatch/issues/2162
-    browser.sessions((sessions) => {
-      if (sessions.status === 0 && sessions.value.length > 0) {
-        const currentSession = sessions.value.find((session) => session.id === browser.sessionId)
+    if (browser.sessionId) {
+      browser.end(() => {
+        done()
+      })
+      return
+    }
 
-        if (currentSession) {
-          browser.end(() => done())
-          return
-        }
-      }
-
-      done()
-    })
+    done()
   })
 }
 
