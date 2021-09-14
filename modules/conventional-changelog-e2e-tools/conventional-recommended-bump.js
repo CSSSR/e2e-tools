2e451e36661d0fe08c145e78a3b491a5c71533fc
Maxim Samoilov <nitive@icloud.com>
2021-09-14 15:14:56 +0300

    Fork conventional-changelog-angular and implement custom bump policy
    1. Bump major if commit contains line “bump major”
    2. Bump minor if commit contains line “bump minor”
    3. Bump patch otherwise

---
+++ modules/conventional-changelog-e2e-tools/conventional-recommended-bump.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const parserOpts = require('./parser-opts')
+
+const levels = {
+  major: 0,
+  minor: 1,
+  patch: 2,
+}
+
+module.exports = {
+  parserOpts,
+
+  whatBump: (commits) => {
+    const bumps = commits.map((commit) => {
+      const bodyLines = (commit.body ?? '').toLowerCase().split('\n').filter(Boolean)
+      if (bodyLines.includes('bump major')) {
+        return levels.major
+      }
+      if (bodyLines.includes('bump minor')) {
+        return levels.minor
+      }
+
+      return levels.patch
+    })
+
+    const majorBumps = bumps.filter((bump) => bump === levels.major).length
+    const minorBumps = bumps.filter((bump) => bump === levels.minor).length
+
+    return {
+      level: Math.min(...bumps),
+      reason: `There are ${majorBumps} major bumps and ${minorBumps} minor bumps`,
+    }
+  },
+}
