ca305874e0195ccc7a04bda6bdd8445efd0eeb01
Maxim Samoilov <nitive@icloud.com>
2021-11-08 14:11:30 +0300

    Исправлен невалидный синтаксис в GitHub Action periodic runs для события successful-deploy

--- modules/tools/src/commands/generate-periodic-runs.js
+++ modules/tools/src/commands/generate-periodic-runs.js
@@ -151,11 +151,11 @@ function generatePeriodicRunWorkflow({ url, command, run, id, config }) {
     }
 
     workflow.jobs['run-tests'].if = [
-      `github.event_name === "workflow_dispatch" ||`,
-      `github.event_name === "deployment_status" &&`,
-      `github.event.deployment_status.state == 'success' && `,
+      `github.event_name == "workflow_dispatch" ||`,
+      `github.event_name == "deployment_status" &&`,
+      `github.event.deployment_status.state == 'success' &&`,
       `github.event.deployment.payload.environmentURL == '${url}'`,
-    ].join('')
+    ].join(' ')
   }
 
   /*
